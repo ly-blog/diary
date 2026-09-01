ebe5936ceb517f9932ef37ff0bb587ce48a4566d
laiyuan <laiyuan@example.com>
2026-09-01 14:48:32 +0800

    1

--- readme/script/fix-punchuation.sh
+++ readme/script/fix-punchuation.sh
@@ -1,10 +1,13 @@
 #!/bin/sh -e
 #1:folder
 
+#爸爸->父亲
+#妈妈->母亲
+#仓库->公司
 
 mySed(){ #1:file
 echo "$i"
-sed -e 's/。/./g' -e 's/：/:/g' -e 's/、/\\/g' -e 's/，/,/g' -e 's/（/(/g' -e 's/）/)/g' -e 's/；/;/g' -e "s/「/'/g" -e "s/」/'/g" -e 's/？/?/g' -i "$1"
+sed -e 's/。/./g' -e 's/：/:/g' -e 's/、/\\/g' -e 's/，/,/g' -e 's/（/(/g' -e 's/）/)/g' -e 's/；/;/g' -e "s/「/'/g" -e "s/」/'/g" -e 's/？/?/g' -e "s/“/\'/g" -e "s/”/\'/g" -e "s/《/'/g" -e "s/》/'/g" -i "$1"
 }
 
 for i in "$1"/*.diary;do
