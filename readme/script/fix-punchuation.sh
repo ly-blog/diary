1dcd7f4999e07a9432c4b8940931abfc3b5cfddd
laiyuan <laiyuan@example.com>
2026-09-02 10:00:18 +0800

    work

--- readme/script/fix-punchuation.sh
+++ readme/script/fix-punchuation.sh
@@ -7,7 +7,7 @@
 
 mySed(){ #1:file
 echo "$i"
-sed -e 's/。/./g' -e 's/：/:/g' -e 's/、/\\/g' -e 's/，/,/g' -e 's/（/(/g' -e 's/）/)/g' -e 's/；/;/g' -e "s/「/'/g" -e "s/」/'/g" -e 's/？/?/g' -e "s/“/\'/g" -e "s/”/\'/g" -e "s/《/'/g" -e "s/》/'/g" -i "$1"
+sed -e 's/。/./g' -e 's/：/:/g' -e 's/、/\\/g' -e 's/，/,/g' -e 's/（/(/g' -e 's/）/)/g' -e 's/；/;/g' -e "s/「/'/g" -e "s/」/'/g" -e 's/？/?/g' -e "s/“/\'/g" -e "s/”/\'/g" -e "s/《/'/g" -e "s/》/'/g" -e 's/…/.../g' -i "$1"
 }
 
 for i in "$1"/*.diary;do
